f421038c5fafa959f267e0ff85c6eee1b6c250a0
tanbirk <tanvir.kashyap879@gmail.com>
2021-12-02 02:06:45 +0530

    ff

--- pages/Login.js
+++ pages/Login.js
@@ -5,6 +5,15 @@ function Login({ providers }) {
     return (
         <div>
            <img className="w-52 mb-5" src="https://icones.pro/wp-content/uploads/2021/04/icone-spotify-rouge.png" alt=""  />
+        
+        {Object.values(providers).map((provider) =>(
+
+            <div key={provider.name}>
+                <button className="bg-[#D2042D]">Login with {provider.name}</button>
+            </div>
+
+        ) )}
+
         </div>
     )
 }
